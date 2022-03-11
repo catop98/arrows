aac15aa621261b76cfa3948796c38eb605784f2a
Camilo Andrés Torres Pérez <camivad@gmail.com>
2022-03-11 12:28:44 -0500

    Add files via upload

---
+++ arrows/practice2.js
@@ -0,0 +1,16 @@
+
+
+    
+
+    $(document).ready(function() {
+
+
+        $('#arrow1').animate({left: 740}, 3000);
+        $('#arrow2').animate({left: 690}, 3000);
+        
+        
+        
+        });
+
+    
+
